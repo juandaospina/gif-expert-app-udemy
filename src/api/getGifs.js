e7d00a2cc050300e930c1de0a3a6ee05e994ecf8
Juan Ospina <juan.ospinal@outlook.com>
2022-07-15 21:44:06 -0500

    get gifs GIPHY

--- src/api/getGifs.js
+++ src/api/getGifs.js
@@ -1,6 +1,6 @@
-const getGifs = async (category) => {
+const getGifs = async ( category ) => {
     const apiKey = "nY1OkmkVO5OPW2OeG1tHaNPuYl3uNzZ4";
-    const res = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${category}&limit=20`)
+    const res = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${category}&limit=5`)
     const { data } = await res.json();
 
     const gifs = data.map( gif => ({
@@ -9,8 +9,8 @@ const getGifs = async (category) => {
         url: gif.images.downsized_medium.url
     }));
 
-    console.log(gifs);
-    return;
+    // console.log(gifs);
+    return gifs;
 }
 
 export default getGifs;
--- src/components/GifGrid/index.jsx
+++ src/components/GifGrid/index.jsx
@@ -1,14 +1,32 @@
+import { useState, useEffect } from 'react';
 import getGifs from '../../api/getGifs';
 
-const GifGrid = (props) => {
+const GifGrid = ( props ) => {
 
     const { category } = props;
+    const [ gifs, setGifs ] = useState([]);
 
-    getGifs(category);
+    const newGifs = async () => {
+        const res = await getGifs(category);
+        setGifs(res);
+    }       
+
+    useEffect(() => {
+        newGifs();
+    }, []);
+
 
     return(
         <>
             <p>{ category }</p>
+
+            <ol>
+                {
+                    gifs.map( ({ id, title }) => (
+                        <li key={id}>{title}</li>
+                    ))
+                }
+            </ol>
         </>
     );
 }
